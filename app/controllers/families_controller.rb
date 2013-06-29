1290769568882f1afdc3d5dff37e7580e55b8bce
a1flecke <aaron.fleckenstein@gmail.com>
2013-06-28 20:06:19 -0500

    Got the ajax piece working.  NExt i need to refactor the partials

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -28,7 +28,7 @@ class FamiliesController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @family }
+      format.js
     end
   end
 
